83340e073c8c6454e57ec2aa73565123c3a0572e
manzomonza <massimiliano.manzo@posteo.ch>
2023-05-03 16:47:27 +0200

    Report Markdown and render script

---
+++ report_render.R
@@ -0,0 +1,20 @@
+## GNXS report render script
+## Add info here
+library(optparse)
+option_list = list(
+  make_option(c("-d", "--dir"), type="character", default=NULL,
+              help="directory containing files to be used for workflow validation", metavar="character"))
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser)
+
+
+dir.create(paste0(opt$dir, "/validation_report"))
+html_filename = paste0(opt$dir, "/GNXS_QC_report/GNXS_report.html")
+
+# rmarkdown::render('~/USB/USB_Diagnostics/GNXS_reports/report.Rmd',
+#                   params = list(diroi = opt$dir),
+#                   output_file = html_filename)
+
+rmarkdown::render('/home/ionadmin/github_app/workflow_validation/report.Rmd',
+                  params = list(diroi = opt$dir),
+                  output_file = html_filename)
